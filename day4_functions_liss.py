ab836837beeb21c2e392d124c62e7762bf52ca1a
snnrsya <syamimienarisya@gmail.com>
2026-01-12 23:57:04 +0800

    day 4: functions and lists

---
+++ day4_functions_liss.py
@@ -0,0 +1,10 @@
+def greet(name):
+  return "hello " + name
+  print(greet("snnrsya"))
+  numbers =[1 ,2 ,3]
+  numbers. append(4)
+  print("numbers list:", numbers)
+  def double_list(1st):
+    return [x*2 for x in 1st]
+    print ("doubled numbers:",
+    double_list(numbers))
